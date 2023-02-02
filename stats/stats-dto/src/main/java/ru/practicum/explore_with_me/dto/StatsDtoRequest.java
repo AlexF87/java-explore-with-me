225314c82461ea80ba984314c324243220c7e745
AlexF87 <feldman87@yandex.ru>
2023-02-02 16:31:32 +0700

    Реализовать HTTP-client

---
+++ stats/stats-dto/src/main/java/ru/practicum/explore_with_me/dto/StatsDtoRequest.java
@@ -0,0 +1,30 @@
+package ru.practicum.explore_with_me.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class StatsDtoRequest {
+    private Long id;
+    @NotNull
+    @NotBlank
+    private String app;
+
+    @NotNull
+    @NotBlank
+    private String uri;
+    @NotNull
+    @NotBlank
+    private String ip;
+
+    @NotNull
+    private LocalDateTime timestamp;
+}
